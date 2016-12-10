46fb1d44a7d583642dd30d19434f77bc2ecbefa1
AlexanderKing\Alexander King <alexander@akingsquest.com>
2016-12-10 13:23:45 -0500

    Added camera follow logic

--- Assets/Resources/Template/Scripts/Toolbox/Camera/CameraFollow.cs
+++ Assets/Resources/Template/Scripts/Toolbox/Camera/CameraFollow.cs
@@ -6,19 +6,23 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public float xMargin = 1f; // Distance in the x axis the player can move before the camera follows.
-    public float yMargin = 1f; // Distance in the y axis the player can move before the camera follows.
+    public float zMargin = 1f; // Distance in the y axis the player can move before the camera follows.
     public float xSmooth = 8f; // How smoothly the camera catches up with it's target movement in the x axis.
-    public float ySmooth = 8f; // How smoothly the camera catches up with it's target movement in the y axis.
-    public Vector2 maxXAndY; // The maximum x and y coordinates the camera can have.
-    public Vector2 minXAndY; // The minimum x and y coordinates the camera can have.
+    public float zSmooth = 8f; // How smoothly the camera catches up with it's target movement in the y axis.
+    public Vector2 maxXAndZ; // The maximum x and y coordinates the camera can have.
+    public Vector2 minXAndZ; // The minimum x and y coordinates the camera can have.
 
-    private Transform m_Player; // Reference to the player's transform.
-
+    public Transform m_Player; // Reference to the player's transform.
+	private float z_offset = 0;
 
     private void Awake()
     {
         // Setting up the reference.
-        m_Player = GameObject.FindGameObjectWithTag("Player").transform;
+        //m_Player = GameObject.FindGameObjectWithTag("Player").transform;
+
+		//Set up offset
+		z_offset = Mathf.Abs(m_Player.position.z - transform.position.z);
+
     }
 
 
@@ -32,7 +36,7 @@ public class CameraFollow : MonoBehaviour
     private bool CheckYMargin()
     {
         // Returns true if the distance between the camera and the player in the y axis is greater than the y margin.
-        return Mathf.Abs(transform.position.y - m_Player.position.y) > yMargin;
+        return Mathf.Abs(transform.position.z - m_Player.position.z) > zMargin;
     }
 
 
@@ -46,7 +50,7 @@ public class CameraFollow : MonoBehaviour
     {
         // By default the target x and y coordinates of the camera are it's current x and y coordinates.
         float targetX = transform.position.x;
-        float targetY = transform.position.y;
+		float targetZ = transform.position.z;
 
         // If the player has moved beyond the x margin...
         if (CheckXMargin())
@@ -59,15 +63,15 @@ public class CameraFollow : MonoBehaviour
         if (CheckYMargin())
         {
             // ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
-            targetY = Mathf.Lerp(transform.position.y, m_Player.position.y, ySmooth*Time.deltaTime);
+			targetZ = Mathf.Lerp(transform.position.z, m_Player.position.z - z_offset, zSmooth*Time.deltaTime);
         }
 
         // The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
-        targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
-        targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+        targetX = Mathf.Clamp(targetX, minXAndZ.x, maxXAndZ.x);
+		targetZ = Mathf.Clamp(targetZ, minXAndZ.y, maxXAndZ.y);
 
         // Set the camera's position to the target position with the same z component.
-        transform.position = new Vector3(targetX, targetY, transform.position.z);
+		transform.position = new Vector3(targetX, transform.position.y, targetZ);
     }
 }
 
